2a37889c1708c99c9463e642e9c3a6bc6c8673a3
Fatima8211 <b95105354@gmail.com>
2025-08-02 16:20:05 +0500

    Add input file

---
+++ Input/input_Data.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    string n, c;
+    int a;
+    cout<<"Name: ";
+    cin>>n;
+    cout<<"Age: ";
+    cin>>a;
+    cout<<"City: ";
+    cin>>c;
+    cout<<"Your Data is: ";
+    cout<<n<<endl;
+    cout<<a<<endl;
+    cout<<c;
+}
--- Print/print_Data.cpp
+++ Print/print_Data.cpp
@@ -5,5 +5,5 @@ int main()
     cout<<"Assalam o Alaikum!"<<endl; 
     cout<<"Name: Fatima Tul Zahra"<<endl; 
     cout<<"Age: 20"<<endl; 
-    cout<<"Goal: I want to become a software engineer"<<endl; 
+    cout<<"Goal: I want to become a software engineer"<<endl;
 }
