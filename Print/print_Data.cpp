db23ce769ddb8d5bdff75d2e878dff0107728719
Fatima8211 <b95105354@gmail.com>
2025-08-02 15:58:41 +0500

    Make folder Print and Input/Output

---
+++ Print/print_Data.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout<<"Assalam o Alaikum!"<<endl; 
+    cout<<"Name: Fatima Tul Zahra"<<endl; 
+    cout<<"Age: 20"<<endl; 
+    cout<<"Goal: I want to become a software engineer"<<endl; 
+}
